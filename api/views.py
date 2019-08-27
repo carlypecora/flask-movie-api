a636bb921011d9ba1593a2ba48bcf8257dfd9917
Carly Pecora <carly@carlypecora.com>
2019-08-27 09:04:56 -0400

    api finished

--- api/views.py
+++ api/views.py
@@ -1,4 +1,6 @@
 from flask import Blueprint, jsonify, request
+from . import db
+from .models import Movie
 
 main = Blueprint('main', __name__)
 
@@ -6,11 +8,17 @@ main = Blueprint('main', __name__)
 def add_movie():
 	movie_data = request.get_json()
 
+	new_movie = Movie(title=movie_data['title'], rating=movie_data['rating'])
+	db.session.add(new_movie)
+	db.session.commit()
 	return 'Done', 201
 
 @main.route('/movies')
 def movies():
-
+	movie_list = Movie.query.all()
 	movies = []
+
+	for movie in movie_list:
+		movies.append({'title' : movie.title , 'rating' : movie.rating})
 
 	return jsonify({'movies' : movies})
