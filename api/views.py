54762dd5f86adbd6c3a8b6fe3d5edd039d07222c
Carly Pecora <carly@carlypecora.com>
2019-08-27 07:59:09 -0400

    create database

--- api/views.py
+++ api/views.py
@@ -1,9 +1,10 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 
 main = Blueprint('main', __name__)
 
 @main.route('/add_movie', methods=['POST'])
 def add_movie():
+	movie_data = request.get_json()
 
 	return 'Done', 201
 
